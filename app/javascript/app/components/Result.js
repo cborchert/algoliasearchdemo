525979993c46ceac2b28e0049ed2560051251a20
Chris Borchert <cborchertrun@gmail.com>
2017-08-24 13:54:56 +0200

    working on layout

--- app/javascript/app/components/Result.js
+++ app/javascript/app/components/Result.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/Result.scss';
+import tinycolor from 'tinycolor2';
 
 //The SearchBar component can be pure -- at this level of an application, we don't need internalized state.
 //We just receive state from the parent and pass the changed value back to the parent
@@ -7,11 +8,14 @@ import '../styles/Result.scss';
 //TODO: proptypes
 export default function Result(props) {
 
-    console.log(props)
+    let theme = tinycolor(props.resultObject.color).isLight() ? 'light' : 'dark',
+        classes = 'result '+'result--'+theme;
     return (
-        <div className="result" style={{background: props.resultObject.color}}>
-            <h2>{props.resultObject.title}</h2>
-            <img src={props.resultObject.image} />
+        <div className={classes} style={{background: props.resultObject.color}}>
+            <div className="result__image-container">
+                <img src={props.resultObject.image} className="result__image"/>
+            </div>
+            <h2 className="result__title">{props.resultObject.title}</h2>
             <h5>genres</h5>
             <ul>
                 {props.resultObject.genre.map((genre, i) => <li key={i}>{genre}</li>)}
